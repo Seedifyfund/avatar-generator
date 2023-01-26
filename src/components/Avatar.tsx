cbb6f01ff330e5247dba0be3dc02e76ae5da9b47
augustorl <augustorldev@gmail.com>
2023-01-26 20:07:26 -0300

    :recycle: refactor avatar component

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,13 +1,22 @@
-import { AvatarImage } from '@/styles'
+import { AvatarProps } from '@/pages';
+import { Backpacks, BaseModel, Hair, Outfits } from '@/styles/avatarStyles';
 
-interface AvatarProps {
-    image?: string;
-}
-const Avatar: React.FC = ({image}: AvatarProps) => {
-
+const Avatar = ({ avatar }: AvatarProps) => {
+    
     return (
         <>
-            <AvatarImage src="/avatar.png" alt="avatar" fill />
+            {avatar ? (
+                <>
+                    <Backpacks src={`static/Male/${avatar?.skin}/${avatar?.archetype}/${avatar?.job}/Backpacks/Male_${avatar?.archetype}_${avatar?.job}_${avatar?.skin}_Backpack_1A.PNG`} />
+                    <BaseModel src={`/static/Male/${avatar?.skin}/Base Model/Skin Base/Male_BaseModel_${avatar?.skin}.png`} />
+                    <Hair src={`/static/Male/${avatar?.skin}/Base Model/Hair/Male_Hair_${avatar?.skin}_1A.png`} />
+                    <Outfits src={`static/Male/${avatar?.skin}/${avatar?.archetype}/${avatar?.job}/Outfits/Male_${avatar?.archetype}_${avatar?.job}_${avatar?.skin}_Clothes_01.png`} />
+                </>
+            ) : (
+                <>
+                    <Hair src="static/Male/Caucasian/Base Model/Hair/Male_Hair_Caucasian_1A.png" />
+                    <BaseModel src={"static/Male/Caucasian/Base Model/Skin Base/Male_BaseModel_Caucasian.png"} /></>
+            )}
         </>
     )
 }
